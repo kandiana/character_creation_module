63c4dbff3e37d8ea18179141d9a550e7c54a0982
diana <kandiana@yandex-team.ru>
2025-05-26 17:38:22 +0300

    add docstrings to main.py and fix linting error

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Выводит результат выбранной атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику'
                 f' равный {5 + randint(3, 5)}')
@@ -17,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Выводит результат выбранной защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -27,6 +29,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Выводит результат спец атаки выбранного класса."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 f' «Выносливость {80 + 25}»')
@@ -38,6 +41,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Управляет началом и концом тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -63,6 +67,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Управляет выбором класса."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -80,8 +85,7 @@ def choice_char_class() -> str:
                   ' Черпает силы из природы, веры и духов.')
         approve_choice = input('Нажми (Y), чтобы подтвердить выбор,'
                                ' или любую другую кнопку,'
-                               ' чтобы выбрать другого персонажа '
-                               ).lower()
+                               ' чтобы выбрать другого персонажа ').lower()
     return char_class
 
 
